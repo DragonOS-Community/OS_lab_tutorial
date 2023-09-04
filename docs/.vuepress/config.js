70680e6d9535675f5afc3585f6e630096f8ae269
Jomo <xuzihao@dragonos.org>
2023-09-04 17:27:50 +0800

    更改目录结构 (#20)
    Co-authored-by: Jomo <13530228968@163.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,52 +1,54 @@
 module.exports = {
-  base: '/OS_lab_tutorial/',
-  title: '操作系统实验',
-  description: 'Welcome!',
-  themeConfig: {
-    nav: [
-		{
-			text: '首页', link: '/'
-		},
-		{
-			text: '实验教程',
-			items: [
-				{ text: '实验1-熟悉类Linux系统', link: '/Lab/Lab1' },
-				{ text: '实验2-进程创建与进程间通信', link: '/Lab/Lab2' },
-				{ text: '实验3-进程调度算法', link: '/Lab/Lab3/' },
-				{ text: '实验4-存储管理算法', link: '/Lab/Lab4/' }, // 这里 /Lab/Lab4/ 表明路径是目录
-				{ text: '实验5-文件管理系统', link: '/Lab/Lab5' },
-				{ text: '实验6-网络编程(暂定)', link: '/Lab/Lab6' }
-			]
-		},
-		{
-			text: '课程练习',
-			items: [
-				{ text: '实验1-熟悉类Linux系统', link: '/Assignment/Lab1' },
-				{ text: '实验2-进程创建与进程间通信', link: '/Assignment/Lab2' },
-				{ text: '实验3-进程调度算法', link: '/Assignment/Lab3' },
-				{ text: '实验4-存储管理算法', link: '/Assignment/Lab4' },
-				{ text: '实验5-文件管理系统', link: '/Assignment/Lab5' },
-				{ text: '实验6-网络编程(暂定)', link: '/Assignment/Lab6' }
-			]
-		},
-		{
-			text: '附录',
-			items: [
-				{ text: '实验1-熟悉类Linux系统', link: '/Appendix/Lab1' },
-				{ text: '实验2-进程创建与进程间通信', link: '/Appendix/Lab2' },
-				{ text: '实验3-进程调度算法', link: '/Appendix/Lab3' },
-				{ text: '实验4-存储管理算法', link: '/Appendix/Lab4' },
-				{ text: '实验5-文件管理系统', link: '/Appendix/Lab5' },
-				{ text: '实验6-网络编程(暂定)', link: '/Appendix/Lab6' }
-			]
-		},
-		{
-			text: '关于我们',
-			link: '/about'
-		}
-	],
-	sidebar: {
-		'/Lab/Lab3/': [
+    base: '/OS_lab_tutorial/',
+    title: '操作系统实验',
+    description: 'Welcome!',
+    themeConfig: {
+      nav: [
+        { text: '首页', link: '/' },
+        {
+          text: 'Linux',
+          items: [
+            { 
+                text: '实验教程', 
+                items:[
+                    { text: '实验1-熟悉类Linux系统', link: '/Linux/Lab/Lab1' },
+				    { text: '实验2-进程创建与进程间通信', link: '/Linux/Lab/Lab2' },
+				    { text: '实验3-进程调度算法', link: '/Linux/Lab/Lab3/' },
+				    { text: '实验4-存储管理算法', link: '/Linux/Lab/Lab4/' }, // 这里 /Lab/Lab4/ 表明路径是目录
+				    { text: '实验5-文件管理系统', link: '/Linux/Lab/Lab5' },
+				    { text: '实验6-网络编程(暂定)', link: '/Linux/Lab/Lab6' }
+                ]
+            },
+            { 
+                text: '课程练习', 
+                items:[
+                    { text: '实验1-熟悉类Linux系统', link: '/Linux/Assignment/Lab1' },
+				    { text: '实验2-进程创建与进程间通信', link: '/Linux/Assignment/Lab2' },
+				    { text: '实验3-进程调度算法', link: '/Linux/Assignment/Lab3' },
+				    { text: '实验4-存储管理算法', link: '/Linux/Assignment/Lab4' },
+				    { text: '实验5-文件管理系统', link: '/Linux/Assignment/Lab5' },
+				    { text: '实验6-网络编程(暂定)', link: '/Linux/Assignment/Lab6' }
+                ]
+            },
+            { 
+                text: '附录', 
+                items:[
+                    { text: '实验1-熟悉类Linux系统', link: '/Linux/Appendix/Lab1' },
+                    { text: '实验2-进程创建与进程间通信', link: '/Linux/Appendix/Lab2' },
+                    { text: '实验3-进程调度算法', link: '/Linux/Appendix/Lab3' },
+                    { text: '实验4-存储管理算法', link: '/Linux/Appendix/Lab4' },
+                    { text: '实验5-文件管理系统', link: '/Linux/Appendix/Lab5' },
+                    { text: '实验6-网络编程(暂定)', link: '/Linux/Appendix/Lab6' }
+                ]
+            }
+          ]
+        },
+        {
+          text: 'DragonOS',link:'/DragonOS'
+        }
+      ],
+      sidebar: {
+		'/Linux/Lab/Lab3/': [
 			{
 				title: '进程调度',
 				children: [
@@ -58,7 +60,7 @@ module.exports = {
 				],
 			},
 		],
-		'/Lab/Lab4/': [
+		'/Linux/Lab/Lab4/': [
 			{
 				title: '存储管理',
 				children: [
@@ -70,5 +72,6 @@ module.exports = {
 			},
 		],
     }
+    }
   }
-}
+  
